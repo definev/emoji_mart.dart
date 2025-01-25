d84e38e1616f5b6746954aa1bb7fae9b81362e3c
definev <daiduong.workmail@gmail.com>
2025-01-25 16:59:57 +0700

    chore: update export for emoji picker component

--- emoji_mart_flutter/lib/emoji_mart_flutter.dart
+++ emoji_mart_flutter/lib/emoji_mart_flutter.dart
@@ -3,6 +3,6 @@ library;
 export 'src/preset.dart';
 export 'src/emoji_picker_theme.dart';
 export 'src/components/emoji.dart';
-export 'src/components/picker.dart';
+export 'src/components/emoji_picker.dart';
 export 'src/components/emoji_mart_inherited_widget.dart';
 export 'package:emoji_mart_data/emoji_mart_data.dart';
